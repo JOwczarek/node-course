2ec76fc26545e11abd72457075d68147ee91613c
Joshua Owczarek <jowczarek@acmtucson.com>
2021-04-04 14:04:17 -0700

    Implemented more of the back end for natours.

---
+++ notes.js
@@ -0,0 +1,13 @@
+/**
+ * ---REST Architecture
+ *
+ * 1. Seperate API into logical resources
+ * 2. Exposed structured, resource-based URLs
+ * 3. Use HTPP methods (verbs)
+ * 4. Usually send data as JSON
+ * 5. Be stateless
+ *
+ *  1. Resource is a representation of something, which has data
+ *      associated with it. (tours, users, reviews)
+ *
+ */
